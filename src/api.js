5ad8ed273a66330df4b7ad1c70815461af556619
sufiyan-calicut <115002412+sufiyan-calicut@users.noreply.github.com>
2023-08-03 10:38:30 +0530

    project completed

--- src/api.js
+++ src/api.js
@@ -1,11 +1,15 @@
-export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
 export const geoApioptions = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1b28cdabaamsh60f558292a63a3cp18f9cejsn89db0204bc26',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "1b28cdabaamsh60f558292a63a3cp18f9cejsn89db0204bc26",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
 };
+const geo = process.env.REACT_APP_GEO_API
+console.log('geo',geo)
+console.log("geo apoi", process.env.REACT_APP_GEO_API);
+console.log("weather apoi", process.env.REACT_APP_WEATHER_API_KEY);
+export const WEATHER_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "9a30dfb79ce30be4c139eefa3e688713";
 
-export const WEATHER_URL = 'https://api.openweathermap.org/data/2.5'
-export const WEATHER_API_KEY = '9a30dfb79ce30be4c139eefa3e688713'
--- src/components/forecast/forecast.js
+++ src/components/forecast/forecast.js
@@ -5,6 +5,7 @@ import {
   AccordionItemHeading,
   AccordionItemPanel,
 } from "react-accessible-accordion";
+import './forecast.css'
 
 const weekDays = [
   "Monday",
@@ -17,11 +18,11 @@ const weekDays = [
 ];
 const Forecast = ({ data }) => {
   const dayInAWeek = new Date().getDay();
- const forecastDays = weekDays
+  const forecastDays = weekDays
     .slice(dayInAWeek, weekDays.length)
     .concat(weekDays.slice(0, dayInAWeek));
 
-    console.log('forecastDays',forecastDays)
+  console.log("forecastDays", forecastDays);
   return (
     <>
       <label className="title">Daily</label>
@@ -38,10 +39,44 @@ const Forecast = ({ data }) => {
                       src={`icons/${item?.weather[0]?.icon}.png`}
                     />
                     <label className="day">{forecastDays[index]}</label>
+                    <label className="description">
+                      {item?.weather[0]?.description}
+                    </label>
+                    <label className="min-max">
+                      {Math.round(item?.main?.temp_min)}°C /{" "}
+                      {Math.round(item?.main?.temp_max)}°C{" "}
+                    </label>
                   </div>
                 </AccordionItemButton>
               </AccordionItemHeading>
-              <AccordionItemPanel></AccordionItemPanel>
+              <AccordionItemPanel>
+                <div className="daily-details-grid">
+                    <div className="daily-details-gridItem">
+                        <label>Pressure</label>
+                        <label>{item.main.pressure}hpa</label>
+                    </div>
+                    <div className="daily-details-gridItem">
+                        <label>Humidity</label>
+                        <label>{item.main.humidity}%</label>
+                    </div>
+                    <div className="daily-details-gridItem">
+                        <label>Clouds</label>
+                        <label>{item.clouds.all}%</label>
+                    </div>
+                    <div className="daily-details-gridItem">
+                        <label>Wind Speed</label>
+                        <label>{item.wind.speed} m/s</label>
+                    </div>
+                    <div className="daily-details-gridItem">
+                        <label>Sea level</label>
+                        <label>{item.main.sea_level} m/s</label>
+                    </div>
+                    <div className="daily-details-gridItem">
+                        <label>Feels like</label>
+                        <label>{Math.round(item.main.feels_like)}°C</label>
+                    </div>
+                </div>
+              </AccordionItemPanel>
             </AccordionItem>
           );
         })}
